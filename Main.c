8f76369f00c69e7b59123294c922c9b438e5d63d
Arthur Stellato <arthurstellato19@gmail.com>
2026-04-13 20:03:56 -0300

    Refactor Main.c to improve person management

--- Main.c
+++ Main.c
@@ -62,96 +62,96 @@ int main()
     
     do{
         printf("Escolha uma opção:\n");
-        printf("1 - Criar pessoa\n");
-        printf("2 - Mostrar Lista\n");
-        printf("3 - Remover pessoa\n");
-        printf("4 - alterar pessoa\n");
-        printf("5 - Sair\n");
+        printf("1 - Create person\n");
+        printf("2 - Show List\n");
+        printf("3 - Remove person\n");
+        printf("4 - Change person\n");
+        printf("5 - Exit\n");
         scanf("%d", &opcao);
 
         switch (opcao){
             
         case 1: // Opção para criar uma nova pessoa e adicioná-la à lista
-            printf("Digite o nome: \n");
+            printf("Enter name: \n");
             getchar();               // Limpa o buffer do teclado
             fgets(nome, 100, stdin); // Lê o nome com espaços
 
-            printf("Digite a idade: ");
+            printf("Enter age: ");
             scanf("%d", &idade);
 
-            printf("Digite o endereço: ");
+            printf("Enter the address: ");
             getchar();                // Limpa o buffer do teclado
             fgets(ender, 100, stdin); // Lê o endereço com espaços
 
             Pessoa *p = p_cria(nome, idade, ender);
             if (p == NULL)
             {
-                printf("Erro ao criar pessoa\n");
+                printf("Error creating person\n");
                 return 1;
             }
             lista[contador++] = p;
             break;
             
-        case 2: // Opção para mostrar a lista de pessoas
-        if (contador == 0){ //Verifica se a lista está vazia
-                printf("Nenhuma pessoa cadastrada!\n");
+        case 2: // Option to show the list of people
+        if (contador == 0){ //Checks if the list is empty
+                printf("No one registered!\n");
                 break;
             }
             for (int i = 0; i < contador; i++){
                 p_acessa(lista[i], nome, &idade, ender);
-                printf("Pessoa %d\n Nome: %s \nIdade: %d \nEndereço: %s", i + 1, nome, idade, ender);
+                printf("Person %d\n Name: %s \nAge: %d \nAddress: %s", i + 1, nome, idade, ender);
             }
             break;
             
-        case 3: // Opção para remover uma pessoa da lista
-            printf("Digite o número da pessoa a ser removida: ");
+        case 3: // Option to remove a person from the list
+            printf("Enter the number of the person to be removed: ");
             int num;
             scanf("%d", &num);
             if (num > 0 && num <= contador)
             {
                 p_libera(lista[num - 1]);
-                for (int i = num - 1; i < contador - 1; i++) // Desloca as pessoas restantes para preencher o espaço vazio
+                for (int i = num - 1; i < contador - 1; i++) // Move the remaining people to fill the empty space.
                 {
                     lista[i] = lista[i + 1];
                 }
                 contador--;
             }
             else{
-                printf("Número inválido!\n");
+                printf("Invalid number!\n");
             }
             break;
             
-        case 4: // Opção para alterar os dados de uma pessoa
-            printf("Digite o número da pessoa a ser alterada: ");
+        case 4: // Option to change a person's data
+            printf("Enter the number of the person to be changed: ");
             scanf("%d", &num);
             if (num > 0 && num <= contador){
-                printf("Digite o novo nome: ");
-                getchar();               // Limpa o buffer do teclado
-                fgets(nome, 100, stdin); // Lê o novo nome com espaços
+                printf("Enter new name: ");
+                getchar();               // Clears the keyboard buffer
+                fgets(nome, 100, stdin); // Read the new name with spaces.
 
-                printf("Digite a nova idade: ");
+                printf("Enter the new age: ");
                 scanf("%d", &idade);
 
-                printf("Digite o novo endereço: ");
-                getchar();                // Limpa o buffer do teclado
-                fgets(ender, 100, stdin); // Lê o novo endereço com espaços
+                printf("Enter the new address: ");
+                getchar();                // Clears the keyboard buffer
+                fgets(ender, 100, stdin); // Read the new address with spaces
                 p_atribui(lista[num - 1], nome, idade, ender);
             }
             else
             {
-                printf("Número inválido!\n");
+                printf("Invalid number!\n");
             }
             break;
             
-        case 5: // Opção para sair do programa
-            printf("Saindo...\n");
+        case 5: // Option to exit the program
+            printf("Leaving...\n");
             for (int i = 0; i < contador; i++)
             {
                 p_libera(lista[i]);
             }
             break;
         default:
-            printf("Opção inválida!\n");
+            printf("Invalid option!\n");
             break;
         }
     } while (opcao != 5);
